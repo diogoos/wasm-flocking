651c61788472730004966ac3d0508eca06ee5e41
daemonleaf <59505963+daemonleaf@users.noreply.github.com>
2022-07-29 10:36:47 -0300

    rust wasm template

---
+++ index.js
@@ -0,0 +1,12 @@
+import('./pkg')
+  .then((m) => {
+    // let app = new m.FlockingApp();
+    
+    // function render() {
+    //   app.draw();
+    //   window.requestAnimationFrame(render);
+    // }
+
+    // render();
+  })
+  .catch(console.error);
